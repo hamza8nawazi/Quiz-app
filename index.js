fd983c0107c3d71de3d9b37ae8499c703385da4d
hamza8nawazi <barkatullahnawazih@gmail.com>
2024-09-02 15:58:22 +0500

    .

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
 const startbtn = document.getElementById('start-btn')
 const nextbtn = document.getElementById('next-btn')
 const quescontainer = document.getElementById('question-container')
-const questions = document.getElementById('question')
+const questionselement = document.getElementById('question')
+const answerbtnelement = document.getElementById('answer-buttons')
+const restartbtn = document.getElementById('restart-btn')
 
 const allques =[
     {
-        question:'what is your name',
-        answer: ['hamza','haris','hitler','hooman'],
-        correctans : 'hamza'
+        question:'1. How many consonants are there in the English alphabet?',
+        answer: ['12','22','21','25'],
+        correctans : '21'
+    },
+    {
+        question: '2. How many days are there in a week?',
+        answer:['12','5','7','8'],
+        correctans: '7'
+    },
+    {
+        question: '3. How many vowels are there in the English alphabet?',
+        answer:['4','6','7','5'],
+        correctans: '5'
+    },
+    {
+        question: '4. How many hours are there in a day?',
+        answer:['12','24','28','49'],
+        correctans: '24'
+    },
+    {
+        question: '5. How many seconds are there in a minute?',
+        answer:['33','66','60','61'],
+        correctans: '60'
     }
 ]
-console.log(allques[0].correctans)
+
+let currentquestion = 0
+
+function startquiz(){
+    quescontainer.style.display= "inline-block"
+    startbtn.style.display ="none"
+    nextbtn.style.display = "inline-block"
+    showques(currentquestion);
+   
+}
+
+function showques(i){
+    questionselement.innerHTML = allques[i].question
+}
+
+
+function next(){
+    currentquestion++
+    if(currentquestion < allques.length){
+        showques(currentquestion)
+    }else{
+        restartbtn.style.display ="inline-block"
+    }
+    
+}
+
+function restartquiz(){
+    
+}
+
