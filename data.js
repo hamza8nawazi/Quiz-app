9db4dabab6beb3c639e8a4473dd608fa72775212
hamza8nawazi <barkatullahnawazih@gmail.com>
2024-09-09 16:59:59 +0500

    .

---
+++ data.js
@@ -0,0 +1,52 @@
+const allques =[
+    {
+        id: 1,
+        question:'1. How many consonants are there in the English alphabet?',
+        answer: [
+            { text: "22", correct: false, id: 'a' },
+            { text: "26", correct: false, id: 'b' },
+            { text: "21", correct: true, id: 'c' },
+            { text: "20", correct: false, id: 'd' },
+          ],
+    },
+    {
+        id: 2,
+        question: '2. How many vowels are there in the English alphabet?',
+        answer:[
+            { text: "4", correct: false, id: 'e' },
+            { text: "6", correct: false, id: 'f' },
+            { text: "7", correct: false, id: 'g' },
+            { text: "5", correct: true, id: 'h' },
+          ],
+    },
+    {
+        id: 3,
+        question: '3. How many hours are there in a day?',
+        answer:[
+            { text: "26", correct: false, id: 'i' },
+            { text: "24", correct: true, id: 'j' },
+            { text: "25", correct: false, id: 'k' },
+            { text: "23", correct: false, id: 'l' },
+          ],
+    },
+    {
+        id: 4,
+        question: '4. How many seconds are there in a minute?',
+        answer:[
+            { text: "60", correct: true, id: 'm' },
+            { text: "66", correct: false, id: 'n' },
+            { text: "76", correct: false, id: 'o' },
+            { text: "65", correct: false, id: 'p' },
+          ],
+    },
+    {
+        id: 5,
+        question: '5. How many days are there in a week?',
+        answer:[
+            { text: "4", correct: false, id: 'q' },
+            { text: "8", correct: false, id: 'r' },
+            { text: "7", correct: true , id: 's' },
+            { text: "9", correct: false, id: 't' },
+          ],
+    },
+]
--- index.js
+++ index.js
@@ -5,68 +5,19 @@ const questionselement = document.getElementById('question')
 const answerbtnelement = document.getElementById('answer-buttons')
 const restartbtn = document.getElementById('restart-btn')
 const correctbtn= document.querySelector('.btn')
-
-
-const allques =[
-    {
-        id: 1,
-        question:'1. How many consonants are there in the English alphabet?',
-        answer: [
-            { text: "22", correct: false, id: 'a' },
-            { text: "26", correct: false, id: 'b' },
-            { text: "21", correct: true, id: 'c' },
-            { text: "20", correct: false, id: 'd' },
-          ],
-    },
-    {
-        id: 2,
-        question: '2. How many days are there in a week?',
-        answer:[
-            { text: "4", correct: false, id: 'a' },
-            { text: "8", correct: false, id: 'b' },
-            { text: "7", correct: true , id: 'c' },
-            { text: "9", correct: false, id: 'd' },
-          ],
-    },
-    {
-        id: 3,
-        question: '3. How many vowels are there in the English alphabet?',
-        answer:[
-            { text: "4", correct: false, id: 'a' },
-            { text: "6", correct: false, id: 'b' },
-            { text: "7", correct: false, id: 'c' },
-            { text: "5", correct: true, id: 'd' },
-          ],
-    },
-    {
-        id: 4,
-        question: '4. How many hours are there in a day?',
-        answer:[
-            { text: "26", correct: false, id: 'a' },
-            { text: "24", correct: true, id: 'b' },
-            { text: "25", correct: false, id: 'c' },
-            { text: "23", correct: false, id: 'd' },
-          ],
-    },
-    {
-        id: 5,
-        question: '5. How many seconds are there in a minute?',
-        answer:[
-            { text: "60", correct: true, id: 'a' },
-            { text: "66", correct: false, id: 'b' },
-            { text: "76", correct: false, id: 'c' },
-            { text: "65", correct: false, id: 'd' },
-          ],
-    }
-]
+const resultcontainer = document.getElementById('result-container')
+const resulttext = document.getElementById('result-text')
 
 let currentquestion = 0
+let score = 0
 
 function startquiz(){
+    score = 0;
     quescontainer.style.display= "inline-block"
     startbtn.style.display ="none"
     nextbtn.style.display = "inline-block"
     restartbtn.style.display = "none"
+     resulttext.style.display = "none"
     currentquestion = 0 // Reset the question index to 0
     showques(currentquestion);
    
@@ -95,26 +46,28 @@ function showans(i){
         answerbtnelement.appendChild(button);
 
         function checkAnswer(selectedAnswer, button) {
-           
+        
             if (selectedAnswer.correct) {
-                button.style.backgroundColor = 'green'
-                alert('correct answer')
-                button.disabled = true
-              
-                
+                score++ ;
+                button.style.backgroundColor = 'green'       
+
           } else {
               button.style.backgroundColor = 'red'
-              alert('wrong answer, try again!')
-              showans(i);
               
           }
+          document.querySelectorAll('.btn').forEach(btn => btn.disabled = true)
+          nextbtn.disabled = false
+          restartbtn.disabled = false
         }
+
+    
     
     }
 }
 
 
 function next(){
+    
     currentquestion++
     
     if(currentquestion < allques.length){
@@ -124,7 +77,18 @@ function next(){
     }else{
         nextbtn.style.display = "none"
         restartbtn.style.display ="inline-block"
+        showResult();
     }
+    
+}
+function showResult() {
+    quescontainer.classList.add('hide') 
+    nextbtn.classList.add('hide') 
+    restartbtn.classList.remove('hide') 
+    resultcontainer.style.display = "inline-block" // Show the result container
+    resulttext.style.display = "inline-block"
+    resulttext.innerHTML = `Quiz Over! Your score is ${score} out of ${allques.length}`
+    quescontainer.style.display = "none"
     
 }
 
